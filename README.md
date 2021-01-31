80ecc69df174f547508a3d4d9aa02e06f42a574a
walkexperience <walkexpereince@gmail.com>
2021-01-31 20:54:02 +0100

    ejercicios

--- README.md
+++ README.md
@@ -8,11 +8,8 @@ Tasca 1 Exercici Nivell 1
 
 optica.sql [REST Countries API](https://restcountries.eu) to pull country data and display it like in the designs.
 
-![Aquí la descripción de la imagen por si no carga](https://raw.githubusercontent.com/parzibyte/WaterPy/Modelos_EntidaRelacion/pizzeria_ER.png)
+![Aquí la descripción de la imagen por si no carga](https://github.com/maestroGit/mysql-estructura/tree/master/Modelos_EntidaRelacion/tienda_ER.png)
 
 ## - Exercici 2 - pizzeria
-![Aquí la descripción de la imagen por si no carga](https://raw.githubusercontent.com/parzibyte/WaterPy/Modelos_EntidaRelacion/pizzeria_ER.png)
+![Aquí la descripción de la imagen por si no carga](https://github.com/maestroGit/mysql-estructura/tree/master/Modelos_EntidaRelacion/pizzeria_ER.png)
 
-
-**Nivell 1.**
-## - Exercici 1 - Òptica
--- pizzeria.sql
+++ pizzeria.sql
@@ -12,10 +12,10 @@ CREATE TABLE IF NOT EXISTS `pizzeria`.`tienda` (  `id_tienda` INT NOT NULL AUTO_
 CREATE TABLE IF NOT EXISTS `pizzeria`.`cliente` (  `id_cliente` INT NOT NULL AUTO_INCREMENT,  `nombre` VARCHAR(25) NOT NULL, `apellido1` VARCHAR(25) NOT NULL,  `apellido2` VARCHAR(25) NOT NULL,  `direccion` VARCHAR(25) NULL DEFAULT NULL, `codigo_postal` INT NULL DEFAULT NULL, `telefono` INT NULL DEFAULT NULL, `id_poblacion` INT NOT NULL,  PRIMARY KEY (`id_cliente`), FOREIGN KEY (`id_poblacion`)    REFERENCES `pizzeria`.`poblacion` (`id_poblacion`));
 CREATE TABLE IF NOT EXISTS `pizzeria`.`pedido` (  `id_pedido` INT NOT NULL AUTO_INCREMENT,  `fecha` DATETIME NULL DEFAULT NULL,  `id_repartidor` INT NOT NULL,  `fecha_entrega` DATETIME NULL DEFAULT NULL,  `precio_total` FLOAT NULL DEFAULT NULL,  `tipo` VARCHAR(25) NULL DEFAULT NULL,  `id_cliente` INT NULL DEFAULT NULL,  `id_tienda` INT NULL DEFAULT NULL, FOREIGN KEY (`id_cliente`)    REFERENCES `pizzeria`.`cliente` (`id_cliente`), FOREIGN KEY (`id_tienda`)    REFERENCES `pizzeria`.`tienda` (`id_tienda`), FOREIGN KEY (`id_cliente`)    REFERENCES `pizzeria`.`cliente` (`id_cliente`), FOREIGN KEY (`id_repartidor`)    REFERENCES `pizzeria`.`empleado` (`id_empleado`), PRIMARY KEY (`id_pedido`));
 
-INSERT INTO provincia (id_provincia, nombre) VALUES (1, 'Barcelona'),(2, 'Tarragona'),(3, 'Gerona'),(4, 'Lerida');
-INSERT INTO poblacion (id_poblacion, nombre, id_provincia) VALUES (1, 'Barcelona',1), (2, 'Granollers',1),(3, 'Reus',2),(4, 'Gerona',3),(5, 'Mollerusa',4);
-INSERT INTO cliente (id_cliente, nombre, apellido1, apellido2, codigo_postal, telefono, id_poblacion) VALUES (1, 'Javier','Maestro','Rodríguez', 08026, 669895369,1);
-INSERT INTO empleado (id_empleado, nombre, apellido1, apellido2, nif, telefono, tipo) VALUES (1, 'Juan', 'López', 'Serrano', 52589625, 659825896, 'cocinero'),(2, 'Luis', 'Pérez', 'Sol', 256958254, 625988957, 'repartidor');
-INSERT INTO producto ( id_producto,  nombre,  descripcion,  precio) VALUES ( 1,  'margarita',  'piza de tomate y queso',  10.2), ( 2,  'big-burguer',  'hamburguesa doble', 9.5);
-INSERT INTO pizza (id_categoria, nombre, id_producto) VALUES (1, 'pizza', 1);
+-- INSERT INTO provincia (id_provincia, nombre) VALUES (1, 'Barcelona'),(2, 'Tarragona'),(3, 'Gerona'),(4, 'Lerida');
+-- INSERT INTO poblacion (id_poblacion, nombre, id_provincia) VALUES (1, 'Barcelona',1), (2, 'Granollers',1),(3, 'Reus',2),(4, 'Gerona',3),(5, 'Mollerusa',4);
+-- INSERT INTO cliente (id_cliente, nombre, apellido1, apellido2, codigo_postal, telefono, id_poblacion) VALUES (1, 'Javier','Maestro','Rodríguez', 08026, 669895369,1);
+-- INSERT INTO empleado (id_empleado, nombre, apellido1, apellido2, nif, telefono, tipo) VALUES (1, 'Juan', 'López', 'Serrano', 52589625, 659825896, 'cocinero'),(2, 'Luis', 'Pérez', 'Sol', 256958254, 625988957, 'repartidor');
+-- INSERT INTO producto ( id_producto,  nombre,  descripcion,  precio) VALUES ( 1,  'margarita',  'piza de tomate y queso',  10.2), ( 2,  'big-burguer',  'hamburguesa doble', 9.5);
+-- INSERT INTO pizza (id_categoria, nombre, id_producto) VALUES (1, 'pizza', 1);
 
